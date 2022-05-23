442ddf02c9a3b872c6ab676719d2d2ed659ac581
unknown <MohammadAnka.ph@gmail.com>
2022-05-23 18:43:12 +0400

    Education Project

---
+++ main.js
@@ -0,0 +1,43 @@
+// change navbar on scroll 
+
+window.addEventListener("scroll" ,() => {
+    document.querySelector('nav').classList.toggle('window-scroll' ,window.scrollY > 0);
+});
+
+// show/hide Faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq-icon i');
+        if(icon.className === 'fas fa-plus'){
+            icon.className = 'fas fa-minus'
+        }else {
+            icon.className = 'fas fa-plus'
+
+        }
+    })
+})
+
+//show/hide nav menu 
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const colseBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    menu.style.display = "flex";
+    colseBtn.style.display ="inline-block";
+    menuBtn.style.display ="none";
+})
+
+// close nav menu 
+const closeNav = () =>{
+    menu.style.display = "none";
+    colseBtn.style.display ="none";
+    menuBtn.style.display ="inline-block";
+}
+colseBtn.addEventListener("click", closeNav)
